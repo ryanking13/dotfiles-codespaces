f2e66e8a7b23ffb4e01d05e0fe1ed5555d952428
Gyeongjae Choi <def6488@gmail.com>
2023-10-17 20:53:16 +0900

    Update install-tools.sh

--- install-tools.sh
+++ install-tools.sh
@@ -15,3 +15,10 @@ chmod +x ~/.local/bin/broot
 rm -rf ~/.local/bin/bat && ln -s /usr/bin/batcat ~/.local/bin/bat
 
 curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
+
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm -rf ~/miniconda3/miniconda.sh
+~/miniconda3/bin/conda init bash
+~/miniconda3/bin/conda init zsh
